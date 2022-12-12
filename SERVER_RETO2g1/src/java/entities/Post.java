e6271f4fa1872ebb0627b80560b4d2092272d55c
henri <henriqueyeguo.yeguo@gmail.com>
2022-12-12 10:05:25 +0100

    Comment & Post Entities annotations and comments

--- SERVER_RETO2g1/src/java/entities/Post.java
+++ SERVER_RETO2g1/src/java/entities/Post.java
@@ -7,80 +7,206 @@ import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * JPA for the entity {@link Post} This class contains this attributes
+ * {@code ID}, {@code content}, {@code publicationDate}, {@code course},
+ * {@code postComments}, {@code image} and {@code video}
+ *
+ *
+ * @author Henrique Yeguo
+ */
+@Entity
+@Table(name = "post", schema = "reto2_g1c_sussybaka")
 public class Post implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
+    /**
+     * ID field for post entity
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer postId;
+
+    /**
+     * Content field, contains the data in the post
+     */
     private String content;
+
+    /**
+     * Publication date field, contains when the post date was created
+     */
     private Timestamp publicationDate;
+
+    /**
+     * Link {@link Course} of the post
+     */
+    @ManyToOne
     private Course course;
+
+    /**
+     * Image field contains the relative path to the image
+     */
     private String image;
+
+    /**
+     * Video field contains the relative path to the video
+     */
     private String video;
 
     /**
+     * List containing all the {@link Comments} a {@link Post} has
+     *
      * @associates <{model.Comment}>
      */
-    private Collection postComments;
+    @OneToMany(mappedBy = "post")
+    private Collection<Comment> postComments;
 
+    /**
+     * {@link Post} class empty constructor
+     */
     public Post() {
         super();
     }
 
+    /**
+     * Gets the post ID
+     *
+     * @return Returns the ID of the post
+     */
     public Integer getPostId() {
         return postId;
     }
 
+    /**
+     * Sets the post ID
+     *
+     * @param postId Passes the ID value of the post
+     */
     public void setPostId(Integer postId) {
         this.postId = postId;
     }
 
+    /**
+     * Gets the content text
+     *
+     * @return content Content
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Sets the content text
+     *
+     * @param content Content
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     * Gets the publication date
+     *
+     * @return publicationDate Publication Date
+     */
     public Timestamp getPublicationDate() {
         return publicationDate;
     }
 
+    /**
+     * Sets the publication date
+     *
+     * @param publicationDate PublicationDate
+     */
     public void setPublicationDate(Timestamp publicationDate) {
         this.publicationDate = publicationDate;
     }
 
+    /**
+     * Gets the course this post is posted
+     *
+     * @return course Course
+     */
     public Course getCourse() {
         return course;
     }
 
+    /**
+     * Sets the course this post is posted
+     *
+     * @param course Course
+     */
     public void setCourse(Course course) {
         this.course = course;
     }
 
+    /**
+     * Gets the image path
+     *
+     * @return image Image
+     */
     public String getImage() {
         return image;
     }
 
+    /**
+     * Sets the image path
+     *
+     * @param image Image
+     */
     public void setImage(String image) {
         this.image = image;
     }
 
+    /**
+     * Gets the video path
+     *
+     * @return video Video
+     */
     public String getVideo() {
         return video;
     }
 
+    /**
+     * Sets the video path
+     *
+     * @param video Video
+     */
     public void setVideo(String video) {
         this.video = video;
     }
 
-    public Collection getPostComments() {
+    /**
+     * Gets the comments that the post has
+     *
+     * @return postComments
+     */
+    public Collection<Comment> getPostComments() {
         return postComments;
     }
 
-    public void setPostComments(Collection postComments) {
+    /**
+     * Sets the comments on the post
+     *
+     * @param postComments PostComments
+     */
+    public void setPostComments(Collection<Comment> postComments) {
         this.postComments = postComments;
     }
 
+    /**
+     * Implementation of the method hashCode, this will return a {@code int}
+     * with the hashCode of this class
+     */
     @Override
     public int hashCode() {
         int hash = 7;
@@ -94,6 +220,14 @@ public class Post implements Serializable {
         return hash;
     }
 
+    /**
+     * This implementation will compare between two objects all the attributes,
+     * if all of them is {@code True} it will return that, if not returns
+     * {@code False}
+     *
+     * @param obj Object
+     * @return boolean True or False if object is equal
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -130,6 +264,11 @@ public class Post implements Serializable {
         return true;
     }
 
+    /**
+     * This implementation returns a concatenated String of all attributes
+     *
+     * @return Concatenated string with all the attributes
+     */
     @Override
     public String toString() {
         return "Post{" + "postId=" + postId + ", content=" + content + ", publicationDate=" + publicationDate + ", course=" + course + ", image=" + image + ", video=" + video + ", postComments=" + postComments + '}';
