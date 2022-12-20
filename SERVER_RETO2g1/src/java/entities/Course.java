b6c20ec1d76b979fd62dabb63e721cdc2cba5bee
Joritz <Joritzjimeno@gmail.com>
2022-12-20 10:13:31 +0100

    Course mappedBy

--- SERVER_RETO2g1/src/java/entities/Course.java
+++ SERVER_RETO2g1/src/java/entities/Course.java
@@ -80,7 +80,7 @@ public class Course implements Serializable {
      * @associates <{Post}>
      * This is a collection with the acctual post of the course
      */
-    @OneToMany(mappedBy = "post")
+    @OneToMany(mappedBy = "course")
     private Collection<Post> coursePosts;
 
     /**
