e74d0ae6b4f034ec0e93beb7e08992705693bc47
henries2022 <henriqueyeguo.yeguo@gmail.com>
2023-01-09 08:31:21 +0100

    Merge origin/server into server
    Conflicts:
    	SERVER_RETO2g1/src/java/ejb/SubjectEJB.java
    	SERVER_RETO2g1/src/java/ejb/UserEJB.java
    	SERVER_RETO2g1/src/java/ejb/interfaces/SubjectEJBLocal.java
    	SERVER_RETO2g1/src/java/entities/Subject.java
    	SERVER_RETO2g1/src/java/entities/User.java
    	SERVER_RETO2g1/src/java/service/ApplicationConfig.java
    	SERVER_RETO2g1/src/java/service/SubjectFacadeREST.java
    	SERVER_RETO2g1/src/java/service/UserFacadeREST.java

--- SERVER_RETO2g1/src/java/server/service/SubjectFacadeREST.java
+++ SERVER_RETO2g1/src/java/server/service/SubjectFacadeREST.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package service;
+package server.service;
 
 import server.ejb.interfaces.SubjectEJBLocal;
 import server.entities.Subject;
