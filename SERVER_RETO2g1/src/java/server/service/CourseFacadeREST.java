886efd5415ef217a67d4e6d1eb991e41f8cf29bd
Joritz <Joritzjimeno@gmail.com>
2023-01-10 08:11:23 +0100

    Interface changed on rest

--- SERVER_RETO2g1/src/java/server/service/CourseFacadeREST.java
+++ SERVER_RETO2g1/src/java/server/service/CourseFacadeREST.java
@@ -10,9 +10,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -22,7 +19,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import server.ejb.CourseEJB;
+import server.ejb.interfaces.CourseEJBLocal;
 import server.exception.CreateException;
 import server.exception.DeleteException;
 import server.exception.ReadException;
@@ -37,7 +34,7 @@ public class CourseFacadeREST {
 
 
     @EJB
-    private CourseEJB ejb;
+    private CourseEJBLocal ejb;
 
     @POST
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
