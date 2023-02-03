9eb8d1d81b8dc663a9e0f17fa5537c2e420fb08c
henries2022 <henriqueyeguo.yeguo@gmail.com>
2023-02-03 08:40:26 +0100

    Add javadoc to comment and post controller interfaces

--- CLIENT_RETO2g1/src/client/logic/CommentController.java
+++ CLIENT_RETO2g1/src/client/logic/CommentController.java
@@ -15,48 +15,59 @@ import javax.ws.rs.core.GenericType;
 public interface CommentController {
 
     /**
+     * This method updates de comment object
      *
-     * @param requestEntity
-     * @throws client.logic.exception.BusinessLogicException
+     *
+     * @param requestEntity The comment object that will be update
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public void edit(Object requestEntity) throws BusinessLogicException;
 
     /**
+     * This method finds a comment by his ID
      *
-     * @param <T>
-     * @param responseType
-     * @param id
-     * @return
-     * @throws client.logic.exception.BusinessLogicException
+     * @param <T> Generic type, it will be comments
+     * @param responseType The object comment as response type
+     * @param id The ID of the comment
+     * @return The comment object found with his ID
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public <T> T find(Class<T> responseType, String id) throws BusinessLogicException;
 
     /**
+     * This method creates a comment
      *
-     * @param requestEntity
-     * @throws client.logic.exception.BusinessLogicException
+     * @param requestEntity The comment object to be created
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public void create(Object requestEntity) throws BusinessLogicException;
 
     /**
+     * Gets all comments from a post
      *
-     * @param <T>
-     * @param responseType
-     * @param id
-     * @return
-     * @throws client.logic.exception.BusinessLogicException
+     * @param <T> The generic type, this will be a comment
+     * @param responseType The reponse object
+     * @param id The ID of the post
+     * @return List with all the comments found in the post
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public <T> T findAll(GenericType<T> responseType, String id) throws BusinessLogicException;
 
     /**
+     * This method removes a comment using his ID
      *
-     * @param id
-     * @throws client.logic.exception.BusinessLogicException
+     * @param id The ID of the comment to be deleted
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public void remove(String id) throws BusinessLogicException;
 
     /**
-     *
+     * Closes the connection
      */
     public void close();
 }
--- CLIENT_RETO2g1/src/client/logic/PostController.java
+++ CLIENT_RETO2g1/src/client/logic/PostController.java
@@ -9,88 +9,109 @@ import client.logic.exception.BusinessLogicException;
 import javax.ws.rs.core.GenericType;
 
 /**
+ * This is a interface to separate the layers between view and models
  *
  * @author Henri
  */
 public interface PostController {
 
     /**
+     * This method returns a List of Post created on that date
      *
-     * @param <T>
-     * @param responseType
-     * @param courseId
-     * @param date
-     * @return
-     * @throws client.logic.exception.BusinessLogicException
+     * @param <T> The returned object, this will probably be a list of post
+     * @param responseType The returned object, this will probably be a list of
+     * post
+     * @param courseId Course ID
+     * @param date Date to filter
+     * @return The returned object, this will probably be a list of post
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public <T> T findByDate(GenericType<T> responseType, String courseId, String date) throws BusinessLogicException;
 
     /**
+     * This method returns a List of Post created on a date range
      *
-     * @param <T>
-     * @param responseType
-     * @param courseId
-     * @param from
-     * @param to
-     * @return
-     * @throws client.logic.exception.BusinessLogicException
+     *
+     * @param <T> The returned object, this will probably be a list of post
+     * @param responseType The returned object, this will probably be a list of
+     * post
+     * @param courseId Course ID
+     * @param from Date start
+     * @param to Date end
+     * @return The returned object, this will probably be a list of post
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public <T> T findByDateRange(GenericType<T> responseType, String courseId, String from, String to) throws BusinessLogicException;
 
     /**
+     * This method returns a Post using a ID
      *
-     * @param <T>
-     * @param responseType
-     * @param id
-     * @return
-     * @throws client.logic.exception.BusinessLogicException
+     * @param <T> The returned object, this will probably be a list of post
+     * @param responseType The returned object, this will probably be a list of
+     * post
+     * @param id Find post by ID
+     * @return Returns a post found by the ID
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public <T> T find(GenericType<T> responseType, String id) throws BusinessLogicException;
 
     /**
+     * This method create a post object
      *
-     * @param requestEntity
-     * @throws client.logic.exception.BusinessLogicException
+     * @param requestEntity The post object to be created on the server
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public void create(Object requestEntity) throws BusinessLogicException;
 
     /**
+     * This method updates a post object
      *
-     * @param requestEntity
-     * @throws client.logic.exception.BusinessLogicException
+     * @param requestEntity The post Object to be updated on the server
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public void edit(Object requestEntity) throws BusinessLogicException;
 
     /**
+     * This method returns a List of Post filtering using the title name
      *
-     * @param <T>
-     * @param responseType
-     * @param title
-     * @param courseId
-     * @return
-     * @throws client.logic.exception.BusinessLogicException
+     * @param <T> This generic type will be a list of Post
+     * @param responseType The object that this method will return
+     * @param title The title to be used as filter
+     * @param courseId The course ID on where to get all the post
+     * @return Returns a List of post
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public <T> T findByTitle(GenericType<T> responseType, String courseId, String title) throws BusinessLogicException;
 
     /**
+     * This method deletes the post using his ID
      *
-     * @param id
-     * @throws client.logic.exception.BusinessLogicException
+     * @param id The ID of the post that will be removed
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public void remove(String id) throws BusinessLogicException;
 
     /**
+     * This gets all the posts from a course
      *
-     * @param <T>
-     * @param responseType
-     * @param courseId
-     * @return
-     * @throws client.logic.exception.BusinessLogicException
+     * @param <T> This generic type will be a list of Post
+     * @param responseType The object that this method will return
+     * @param courseId The course ID on where to get all the post
+     * @return Returns a List of post
+     * @throws client.logic.exception.BusinessLogicException Exception if
+     * something went wrong
      */
     public <T> T getCoursePosts(GenericType<T> responseType, String courseId) throws BusinessLogicException;
 
     /**
-     *
+     * Closes the connection
      */
     public void close();
 
